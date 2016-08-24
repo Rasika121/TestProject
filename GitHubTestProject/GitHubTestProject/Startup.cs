7ae16b8b5283732ae4924e6b9c44463994f5610f
Rasika121 <rasikagaikwad1@gmail.com>
2016-08-24 22:13:52 +0530

    First Test Project commit

---
+++ GitHubTestProject/GitHubTestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTestProject.Startup))]
+namespace GitHubTestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
